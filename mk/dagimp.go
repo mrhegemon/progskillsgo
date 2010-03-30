903ae889d3223af315fd2ee5907075e4d502c609
tymlipari@yahoo.com <tymlipari@yahoo.com@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-03-30 01:11:59 +0000

    nano

---
+++ mk/dagimp.go
@@ -0,0 +1,15 @@
+package dag
+
+type SetImp struct {
+	setmap map[string]string
+}
+
+func(s *SetImp) Get(name string) Target {
+	return setmap[name]
+}
+
+func(s *SetImp) Apply(t Target, a Action) os.Error {
+	if err := t.ApplyPreq(a); err != nil { return err }
+	if err := t.Apply(a); err != nil { return err }
+	return nil
+}
