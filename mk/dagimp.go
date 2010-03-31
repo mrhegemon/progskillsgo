201a65d1a335c1db6a3c2261de2516b9b2303ede
tymlipari@yahoo.com <tymlipari@yahoo.com@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-03-31 17:36:01 +0000

    added more to dagimp.go

--- mk/dagimp.go
+++ mk/dagimp.go
@@ -17,19 +17,12 @@ func(s *SetImp) Apply(t Target, a Action) os.Error {
 }
 
 func(s *SetImp) AddFile(fname string, t TargetFactory) (string, os.Error) {
-	file, err := os.Open(s, os.O_RDONLY, 444)
+	file, err := os.Open(fname, os.O_RDONLY, 444)
 	if err != nil { return _, err }
 
-	//this function needs to read each non-tabbed line from the file
-	//and add that line to slice of strings
+		file
 
-	//then it passes everything to the target factory, which will
-	//return the targets.
-
-	//Return the name of the first target, and nil for the error.
-	//If an error is encountered along the way, return _ for the string
-	//and the error.
-	return _, nil
+	return AddString(str, t)
 }
 
 func(s *SetImp) AddString(doc string, t TargetFactory) (string, os.Error) {
@@ -55,7 +48,7 @@ func(s *SetImp) Add(lines []string, t TargetFactory) (string, os.Error) {
 
 func(s *SetImp) Put(t Target) (string, os.Error) {
 	fromMap := s.Get(t.Name)
-	if fromMap == nil {
+	if fromMap != nil {
 		fromMap.Merge(t)
 	} else { s.setmap[t.Name] = t }
 	
