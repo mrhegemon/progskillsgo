9e0a945e365b2cb5b0e007bf3ce249eac2eb87a3
hegemon.bill <hegemon.bill@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-07 20:22:58 +0000

    appended info for mk

--- mk/doc.go
+++ mk/doc.go
@@ -5,9 +5,17 @@ Authors: William Broza, Tym Lipari
 dag reads in a mkfile, makes a directed acyclic graph and displays the order of
 that graph. 
 
+mk uses dag to read in the mkfile, the directed acyclic graph created is then 
+used to build a list of commands that are fed into the the shell to be executed.
+
 Usage:
   6/dag [-f mkfile] [command]
 
 dag can take in a makefile or a list of file names
+
+	6/mk [-f mkfile] [command]
+
+mk can take in a makefile or a list of file names
+
 */
 package documentation
