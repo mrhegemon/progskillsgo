e4dea8f003beb3ab1ccc9b60f93920b9955ebec3
hegemon.bill <hegemon.bill@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-12 20:27:46 +0000

    I like candy. Added stuff.

---
+++ trunk/matrix/test.go
@@ -0,0 +1,20 @@
+/*
+Authors: William Broza, Tym Lipari
+Matrix Testing program
+
+tests add and mult matrices
+
+usage:
+	test
+*/
+package main
+
+import ("fmt"; "os"; "strconv"; "flag"; "container/vector")
+
+//program main
+func main(){
+
+
+
+}
+
