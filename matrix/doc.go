f8e686cff047aad88749a4be7702bf9b9192f6d9
hegemon.bill <hegemon.bill@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-12 18:44:20 +0000

    I am so damn lazy.

---
+++ matrix/doc.go
@@ -0,0 +1,20 @@
+/*
+
+Authors: William Broza, Tym Lipari
+
+matrix
+
+mk uses dag to read in the mkfile, the directed acyclic graph created is then 
+used to build a list of commands that are fed into the the shell to be executed.
+
+Usage:
+  6/dag [-f mkfile] [command]
+
+dag can take in a makefile or a list of file names
+
+	6/mk [-f mkfile] [command]
+
+mk can take in a makefile or a list of file names
+
+*/
+package documentation
