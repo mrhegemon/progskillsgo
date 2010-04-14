e50a47d9724fc563ee7cc8dfb7ebffaf0824c36c
hegemon.bill <hegemon.bill@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-14 00:12:19 +0000

    This code is totally untested but perfect in every way.

--- trunk/matrix/matrix.go
+++ trunk/matrix/matrix.go
@@ -1,23 +1,22 @@
-
 /*
 Authors: William Broza, Tym Lipari
-Matrix addition and  multiplacation
+Mmatrix addition and  multiplacation
 
-atrix evaluates addition and multiplacation of matrices
+matrix evaluates addition and multiplacation of matrices
 */
 
-package atrix
+package matrix
 
 import ("fmt"; "os"; "container/vector")
 
-// Implements a atrix.
-type atrix struct {
+// Implements a matrix.
+type matrix struct {
 		x int
 		y int
 		mtx []*Vector
 }
 
-func(m *atrix) build(int x, int y) {
+func(m *matrix) build(int x, int y) {
 	m.x = x
 	m.y = y
 	m.mtx = make([] *Vector, x)
@@ -29,44 +28,41 @@ func(m *atrix) build(int x, int y) {
 	}
 }
 
-func (m *atrix) set(int x, int y, float v) {
+func (m *matrix) set(int x, int y, float v) {
 	m.mtx[x].Set(y, v)
 }
 
-func (m *atrix) get(int x, int y) float {
+func (m *matrix) get(int x, int y) float {
 	return m.mtx[x].At(y)
 }
 
-func add(atrix A, atrix B) (atrix, os.Error) {
+func add(matrix A, matrix B) (matrix, os.Error) {
 	if (A.x != B.x || A.y != B.y) {
-		return nil, os.NewError("Matrixes not same dimensions")
+		return nil, os.NewError("matrixes are not same dimensions")
 	}
-
-	atrix rm;
+	matrix rm;
 	rm.build(B.x, A.y)
 	float result = 0;
 
 	for int row = 0; row < B.x; row++ {
 		for int col = 0; col < A.y;	col++ {
-			result = A.At(row, col) + B.At(row, col)
+			result = A.get(row, col) + B.get(row, col)
 			rm.set(row, col, result) 
 		}
 	}
-	return rm, _
+	return rm, nil
 }
 
-func mult(atrix A, atrix B) (atrix, os.Error) {
-	if (A.x != B.x || A.y != B.y) {
-		os.Error err
-		return _, err
+func mult(matrix A, matrix B) (matrix, os.Error) {
+	if (B.x != A.y) {
+		return nil,  os.NewError("matrices are not the correct dimensions")
 	}
-
-	atrix rm;
-	rm.build(B.x, A.y)
+	matrix rm;
+	rm.build(A.x, B.y)
 	float result = 0;
 
-	for int row = 0; row < B.x; row++ {
-		for int col = 0; col < A.y; col++ {
+	for int row = 0; row < A.x; row++ {
+		for int col = 0; col < B.y; col++ {
     	result = 0
      	for int k = 0; k < B.y; k++) {
       	result = result + A.get(row, k)*B.get(k, col)
@@ -74,6 +70,50 @@ func mult(atrix A, atrix B) (atrix, os.Error) {
 			rm.set(row, col, result)
 		}
 	}
-	return rm, _
+	return rm, nil
 }
+
+func add(matrix A, matrix B) (matrix, os.Error) {
+	if (A.x != B.x || A.y != B.y) {
+		return nil, os.NewError("matrices are not same dimensions")
+	}
+	matrix rm;
+	rm.build(B.x, A.y)
+	float result = 0;
+	for int row = 0; row < B.x; row++ {
+		for int col = 0; col < A.y;	col++ {
+			result = A.get(row, col) + B.get(row, col)
+			rm.set(row, col, result) 
+		}
+	}
+	return rm, nil
+}
+
+func (m *matrix) print(matrix A) os.Error {
+	if A == nil {
+		return os.NewError("matrix is null")
+	} else {
+		for int row = 0; row < A.y; row++ {
+			for int col = 0; col < A.x;	col++ {
+				fmt.print(A.get(row, col))
+				//might already print a space
+				fmt.print(" ")
+			}
+		fmt.println()
+		}
+		return nill
+	}
+}
+
+func (m *matrix) buildTestMatrix(int x, int y, float genNum, float genNum2) matrix {
+	m.build(x, y)
+	for int a = 0; a > x; a++ {
+		for int b = 0; b > y; b++ {
+			m.set(a, b, genNum)
+			genNum = (genNum2 * 1.23456789) / (2 /genNum)
+		}
+	}
+	return tm
+}
+
 
