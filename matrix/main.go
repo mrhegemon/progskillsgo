22d0fe98d795046f6d937601c2ae5b3d848d9734
hegemon.bill <hegemon.bill@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-14 19:32:45 +0000

    Compiles!

--- matrix/main.go
+++ matrix/main.go
@@ -8,54 +8,79 @@ tests add and mult matrices
 usage:
 	matrix
 */
-package matrix
+package main
 
-import ("fmt")
+import ("fmt"; "matrix")
 
 //program main
-func main(){
-	m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12 := new(matrix.matrix)
+func main() {
+	//test matrices
+	m1 := new(matrix.Matrix) 
+	m2 := new(matrix.Matrix) 
+	m3 := new(matrix.Matrix) 
+	m4 := new(matrix.Matrix) 
+	m5 := new(matrix.Matrix) 
+	m6 := new(matrix.Matrix) 
+	m7 := new(matrix.Matrix) 
+	m8 := new(matrix.Matrix) 
+	m9 := new(matrix.Matrix) 
+	m10 := new(matrix.Matrix) 
+	m11 := new(matrix.Matrix)
+	m12 := new(matrix.Matrix)
+	//return matrices
+	rm1 := new(matrix.Matrix) 
+	rm2 := new(matrix.Matrix) 
+	rm3 := new(matrix.Matrix) 
+	rm4 := new(matrix.Matrix) 
+	rm5 := new(matrix.Matrix) 
+	rm6 := new(matrix.Matrix) 
 
+	//TEST 1
 	fmt.Print("add test 1")
-	m1.buildTestMatrix(10, 7, 24.68, 13.579)
-	m2.buildTestMatrix(10, 7, 90.36, 179.2)
+	m1.BuildTestMatrix(10, 7, 24.68, 13.579)
+	m2.BuildTestMatrix(10, 7, 90.36, 179.2)
 
-	rm1 = add(m1, m2)
+	rm1, _ = matrix.Add(m1, m2)
 	rm1.Print()
 
+	//TEST 2
 	fmt.Print("mult test 1")
-	m3.buildTestMatrix(4, 7, 24.068, 13.579)
-	m4.buildTestMatrix(7, 11, 24.868, 13.579)
+	m3.BuildTestMatrix(4, 7, 24.068, 13.579)
+	m4.BuildTestMatrix(7, 11, 24.868, 13.579)
 
-	rm2 = mult(m3, m4)
+	rm2, _ = matrix.Mult(m3, m4)
 	rm2.Print()
 
+	//TEST 3
 	fmt.Print("add test 2")
-	m5.buildTestMatrix(5, 30, 24.65, 5.579)
-	m6.buildTestMatrix(5, 30, 14.68, 153.54)
+	m5.BuildTestMatrix(5, 30, 24.65, 5.579)
+	m6.BuildTestMatrix(5, 30, 14.68, 153.54)
 
-	rm3 = add(m5, m6)
+	rm3, _ = matrix.Add(m5, m6)
 	rm3.Print()
 
+	//TEST 4
 	fmt.Print("mult test 2")
-	m7.buildTestMatrix(2, 100, 234.68, 1390.0)
-	m8.buildTestMatrix(100, 17, 4.68, 13579.0)
+	m7.BuildTestMatrix(2, 100, 234.68, 1390.0)
+	m8.BuildTestMatrix(100, 17, 4.68, 13579.0)
 
-	rm4 = mult(m7, m8)
+	rm4, _ = matrix.Mult(m7, m8)
 	rm4.Print()
 
+	//TEST 5
 	fmt.Print("return add error")
-	m9.buildTestMatrix(5, 31, 24.65, 5.579)
-	m10.buildTestMatrix(5, 30, 14.68, 153.54)
+	m9.BuildTestMatrix(5, 31, 24.65, 5.579)
+	m10.BuildTestMatrix(5, 30, 14.68, 153.54)
 
-	rm5 = add(m9, m10)
+	rm5, _ = matrix.Add(m9, m10)
 	rm5.Print()
 
+	//TEST 6
 	fmt.Print("return mult error")
-	m11.buildTestMatrix(2, 10, 234.68, 1390.0)
-	m12.buildTestMatrix(100, 17, 4.68, 13579.0)
+	m11.BuildTestMatrix(2, 10, 234.68, 1390.0)
+	m12.BuildTestMatrix(100, 17, 4.68, 13579.0)
 
-	rm6 = mult(m11, m12)
+	rm6, _ = matrix.Mult(m11, m12)
 	rm6.Print()
 }
 
--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,119 +1,125 @@
 /*
 Authors: William Broza, Tym Lipari
-Mmatrix addition and  multiplacation
+Matrix addition and  multiplacation
 
-matrix evaluates addition and multiplacation of matrices
+Matrix evaluates addition and multiplacation of matrices
 */
 
 package matrix
 
 import ("fmt"; "os"; "container/vector")
 
-// Implements a matrix.
-type matrix struct {
+// Implements a Matrix.
+type Matrix struct {
 		mrow int
 		mcol int
-		mtx []*Vector
+		mtx []*vector.Vector
 }
 
-func(m *matrix) getRow() int {
+func(m *Matrix) GetRow() int {
 	return m.mrow
 }
 
-func(m *matrix) getCol() int {
+func(m *Matrix) GetCol() int {
 	return m.mcol
 }
 
-func(m *matrix) setRow(int x) {
+func(m *Matrix) SetRow(x int) {
 	m.mrow = x
 }
 
-func(m *matrix) setCol(int y) int {
+func(m *Matrix) SetCol(y int) {
 	m.mcol = y
 }
 
-func(m *matrix) build(int x, int y) {
-	m.setRow(x)
-	m.setCol(y)
-	m.mtx = make([] *Vector, x)
+func(m *Matrix) Build(x int, y int) {
+	m.SetRow(x)
+	m.SetCol(y)
+	m.mtx = make([] *vector.Vector, x)
 
 	for _, a := range m.mtx {
-		a = new(Vector)
-		for b := 0; b < y; b++{
-			a.Push(0);
+		a = new(vector.Vector)
+		for b := 0; b < y; b++ {
+			a.Push(0)
+		}
 	}
 }
 
-func (m *matrix) set(int x, int y, float v) {
+func(m *Matrix) Set(x int, y int, v float) {
 	m.mtx[x].Set(y, v)
 }
 
-func (m *matrix) get(int x, int y) float {
-	return m.mtx[x].At(y)
+func(m *Matrix) Get(x int, y int) float {
+	return m.mtx[x].At(y).(float)
 }
 
-func add(matrix A, matrix B) (matrix, os.Error) {
-	if (A.getRow() != B.getRow() || A.getCol() != B.getCol()) {
-		return nil, os.NewError("matrixes are not same dimensions")
-	}
-	matrix rm;
-	rm.build(B.getRow(), A.getCol())
-	float result = 0;
+func Add(A *Matrix, B *Matrix) (*Matrix, os.Error) {
+	if A.GetRow() != B.GetRow() || A.GetCol() != B.GetCol() {
 
-	for int row = 0; row < B.getRow(); row++ {
-		for int col = 0; col < A.getCol();	col++ {
-			result = A.get(row, col) + B.get(row, col)
-			rm.set(row, col, result) 
+		//will not accept nil!! FIX
+		return A, os.NewError("Matrixes are not same dimensions")
+	}
+	rm := new(Matrix)
+	rm.Build(B.GetRow(), A.GetCol())
+	var result float
+	result = 0.0
+	for row := 0; row < B.GetRow(); row++ {
+		for col := 0; col < A.GetCol();	col++ {
+			result = A.Get(row, col) + B.Get(row, col)
+			rm.Set(row, col, result) 
 		}
 	}
 	return rm, nil
 }
 
-func mult(matrix A, matrix B) (matrix, os.Error) {
-	if (B.getRow() != A.getCol()) {
-		return nil,  os.NewError("matrices are not the correct dimensions")
+func Mult(A *Matrix, B *Matrix) (*Matrix, os.Error) {
+	if (B.GetRow() != A.GetCol()) {
+		
+		//will not accept nil!! FIX
+		return A,  os.NewError("matrices are not the correct dimensions")
 	}
-	matrix rm;
-	rm.build(A.getRow(), B.getCol())
-	float result = 0;
-
-	for int row = 0; row < A.getRow(); row++ {
-		for int col = 0; col < B.getCol(); col++ {
-    	result = 0
-     	for int k = 0; k < B.y; k++) {
-      	result = result + A.get(row, k)*B.get(k, col)
+	rm := new(Matrix)
+	rm.Build(A.GetRow(), B.GetCol())
+	var result float
+	result = 0.0
+	for row := 0; row < A.GetRow(); row++ {
+		for col := 0; col < B.GetCol(); col++ {
+    	result = 0.0
+     	for k := 0; k < B.GetCol(); k++ {
+      	result = result + A.Get(row, k)*B.Get(k, col)
 			}
-			rm.set(row, col, result)
+			rm.Set(row, col, result)
 		}
 	}
 	return rm, nil
 }
 
-func (m *matrix) print(matrix A) os.Error {
-	if A == nil {
-		return os.NewError("matrix is null")
+func(m *Matrix) Print() os.Error {
+	if m == nil {
+		return os.NewError("Matrix is null")
 	} else {
-		for int row = 0; row < A.y; row++ {
-			for int col = 0; col < A.x;	col++ {
-				fmt.print(A.get(row, col))
+		for col := 0; col < m.GetCol(); col++ {
+			for row := 0; row < m.GetRow();	row++ {
+				fmt.Print(m.Get(row, col))
 				//might already print a space
-				fmt.print(" ")
+				fmt.Print(" ")
 			}
-		fmt.println()
+		fmt.Println()
 		}
-		return nill
 	}
+	return nil
 }
 
-func (m *matrix) buildTestMatrix(int x, int y, float genNum, float genNum2) matrix {
-	m.build(x, y)
-	for int a = 0; a > x; a++ {
-		for int b = 0; b > y; b++ {
-			m.set(a, b, genNum)
+func (m *Matrix) BuildTestMatrix(x int, y int, genNum float, genNum2 float) *Matrix {
+	rm := new(Matrix)
+	rm.Build(x, y)
+	for a := 0; a > x; a++ {
+		for b := 0; b > y; b++ {
+			rm.Set(a, b, genNum)
 			genNum = (genNum2 * 1.23456789) / (2 /genNum)
 		}
 	}
-	return tm
+	return rm
 }
 
 
