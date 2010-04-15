1e0e70b0700e3e420929427bcad12000c83e308d
tymlipari@yahoo.com <tymlipari@yahoo.com@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-15 20:47:53 +0000

    more functionality for list

--- wagon/list.go
+++ wagon/list.go
@@ -6,14 +6,18 @@ struct node {
 }
 
 func(this *node) linkToFront(n *node) {
-	n.linkToFront(this.prev)
-	n.linkToBack(this)
+	if n != nil {
+		n.linkToFront(this.prev)
+		n.linkToBack(this)
+	}
 	this.prev = n
 }
 
 func(this *node) linkToBack(n *node) {
-	n.linkToFront(this)
-	n.linkToBack(this.next)
+	if n != nil {
+		n.linkToFront(this)
+		n.linkToBack(this.next)
+	}
 	this.next = n
 }
 
@@ -23,8 +27,46 @@ func(this *node) setValue(value {}interface) {}interface {
 	return old
 }
 
+func(this *node) getValue() {}interface { return this.val }
+
 func newNode(val {}interface) *node{
 	newNode := new(node)
 	newNode.setValue(val)
 }
+
+
+struct LinkedList {
+	head, tail *node
+	length int
+}
+
+func (this *LinkedList) Len() int { return this.length }
+func (this *LinkedList) Init() { this.length = 0; head, tail = nil }
+func (this *LinkedList) Front() {}interface { return this.head.getValue() }
+func (this *LinkedList) Back() {}interface { return this.tail.getValue() }
+func (this *LinkedList) PushFront(val {}interface) {
+	nnode := newNode(val)
+
+	if this.head == nil {
+		this.tail = nnode
+	} else {
+		nnode.linkToBack(this.head)
+	}
+	this.head = nnode
+	this.length++
+}
+func (this *LinkedList) PushBack(val {}interface) {
+	nnode := newNode(val)
+
+	if this.head == nil {
+		this.head = nnode
+	} else {
+		nnode.linkToFront(this.tail)
+	}
+	this.tail = nnode
+	this.length++
+}
+
+
+
 
