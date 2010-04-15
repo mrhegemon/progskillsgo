2e575f4b7037887d6f1ed585395718f520b8dc2e
tymlipari@yahoo.com <tymlipari@yahoo.com@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-15 21:01:15 +0000

    functionally complete linked list

--- trunk/wagon/list.go
+++ trunk/wagon/list.go
@@ -1,5 +1,11 @@
 package list
 
+var( RANGE_ERROR os.Error)
+
+func init(){
+	RANGE_ERROR = os.NewError("LinkedList:  Index out of range.")
+}
+
 struct node {
 	val {}interface
 	prev, next *node
@@ -66,6 +72,55 @@ func (this *LinkedList) PushBack(val {}interface) {
 	this.tail = nnode
 	this.length++
 }
+func(this *LinkedList) Remove(index int) {}interface, os.Error {
+	if index < 0 || index >= this.length {
+		return nil, RANGE_ERROR
+	}
+
+	//special cases (index = 0 or length-1)
+	if index == 0 {
+		this.head.next.linkToFront(nil)
+		this.head = this.head.next
+	} else if index == this.length - 1 {
+		this.tail.prev.linkToBack(nil)
+		this.tail = this.tail.prev
+	}
+
+	tempNode := this.head
+	for y := 1; y <= index; y++ {
+		tempNode = tempNode.next
+	}
+
+	tempNode.prev.linkToBack(tempNode.next)
+	tempNode.next.linkToFront(tempNode.prev)
+
+	this.length--
+
+	return tempNode.getValue(), nil
+}
+func(this *LinkedList) At(index int) {}interface, os.Error {
+	if index < 0 || index >= this.length {
+		return nil, RANGE_ERROR 
+	}
+	
+	if index < (this.length / 2) {
+		tempNode := this.head
+		for y:= 1; y < this.length; y++ {
+			tempNode = tempNode.next
+		}
+		return tempNode.getValue(), nil
+	} else {
+		tempNode := this.tail
+		for y:= this.length - 2; y > 0; y-- {
+			tempNode = tempNode.prev
+		}
+		return tempNode.getValue(), nil
+	}
+
+	return nil, os.NewError("LinkedList:  Search Error")
+}
+
+
 
 
 
