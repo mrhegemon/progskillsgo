5a65cebb398455e9dcd8c48f2fe59458a4c7cb56
hegemon.bill <hegemon.bill@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-21 20:14:56 +0000

    I DO ALL THE WORK

--- trunk/wagon/wagonmain.go
+++ trunk/wagon/wagonmain.go
@@ -9,13 +9,51 @@ usage:
 */
 package main
 
-import "wagon"
+import ("wagon"; "os"; "strconv")//; "bytes")
 
 func main() {
-	wagon_game.NewGame(20, 20)
+	var row, col int
+	var err os.Error
+
+	if len(os.Args) != 3 {
+		println("USAGE:   wagon [rows] [cols]")
+		os.Exit(-1)
+	} else {
+			if row, err = strconv.Atoi(os.Args[1]); err != nil {
+					println(err.String())
+					os.Exit(-1)
+			}
+			if col, err = strconv.Atoi(os.Args[2]); err != nil {
+					println(err.String())
+					os.Exit(-1)
+			}
+	}
+	
+
+	//set up stty
+	_, err = os.ForkExec("/bin/stty", []string{"cbreak"}, os.Environ(), "", nil)
+	if err != nil {
+		println(err.String())
+		os.Exit(-1)
+	}
+
+	inp := make([]byte, 4)
+	os.Stdin.Read(inp)
+	charString := string(inp)
+  chars := []int(charString)
+
+	wagon_game.NewGame(row, col)
 	wagon_game.Print()
+	for chars[0] != 'q' && chars[0] != 'Q' {
+		wagon_game.Act(chars[0])
+		wagon_game.Print()
 
 
+		//refresh the input
+		os.Stdin.Read(inp)
+		charString = string(inp)
+  	chars = []int(charString)
+	}
 /*
 	println("\033[7m Linux OS! Best OS!! \033[0m")
 	println("\033[31m I am in Red")
@@ -25,3 +63,5 @@ func main() {
 	println("\033c")
 */
 }
+
+
