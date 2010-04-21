8321a35d12dc0ef7ad3176202a766cc28b3a7954
hegemon.bill <hegemon.bill@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-21 06:02:49 +0000

    dumb updates

--- wagon/wagonmain.go
+++ wagon/wagonmain.go
@@ -10,5 +10,15 @@ usage:
 package main
 
 func main() {
-
+		println("\033clear")
+		println("\033[2J")
+		println("\033[8u")
+		println("\033[N2")
+		println("\033[8u")
+		println("\033[7m Linux OS! Best OS!! \033[0m")
+		println("\033[31m I am in Red")
+		println("\033[5;10H Hello")
+		println("\033[7s")
+		println("\033[7m Linux OS! Best OS!! \033[0m")
+		println("\033[31m I am in Red")
 }
