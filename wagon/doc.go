65017658018abdff5e7b04d32ed2731e113324ba
hegemon.bill <hegemon.bill@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-21 20:59:42 +0000

    Added some docs.

---
+++ wagon/doc.go
@@ -0,0 +1,22 @@
+/*
+
+This package contains wagonmain.go, wagon.go and list.go
+
+Authors: William Broza, Tym Lipari
+
+list.go contains the package list for creating, adding, and removing
+elements from a list.
+
+wagon.go contains the package list for creating, adding, and removing
+wagons from a wagon train using a linkedList.
+
+wagonmain.go contains the main for the wagon game.
+
+To run wagonmain.go, run make using makefile.  Then run wagon with 2 arguments.
+The first argument sets the row length for the game.
+The second argument sets the column length for the game.
+
+	Example: wagon 20 10
+
+*/
+package documentation
