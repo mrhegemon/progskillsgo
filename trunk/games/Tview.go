0ed2fc573c2f9337b8d9b72132feea5ad4775565
tymlipari@yahoo.com <tymlipari@yahoo.com@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-28 19:17:17 +0000

    Finished generic view. stored in Tview.go

--- trunk/games/Tview.go
+++ trunk/games/Tview.go
@@ -1,3 +1,7 @@
+                                                                     
+                                                                     
+                                                                     
+                                             
 /*
 Authors: William Broza, Tym Lipari
 Tick Tack Toe game
@@ -15,27 +19,32 @@ package main
 import "os"
 import "io"
 import "fmt"
-import "reflect"
 
 type GView struct {
 	inOut io.ReadWriter
 	name, other string
 	refComm chan string
+	instr chan string
+	directions string
 }
 
-//bool gametype = false; //false for RPS, True for TTT
-
-func NewGView(inout io.ReadWriter, n string, ref chan string) *RPSView {
+//inout = input/output
+//n = this view's name
+//dir = the directions for this game
+//ref = the channel that Loop() will communicate with
+func NewGView(inout io.ReadWriter, n, dir string, ref chan string) *GView {
 	view := new(GView)
 	view.inOut = inout
 	view.name = n
 	view.other = ""
 	view.refComm = ref
+	view.instr = ""
+	view.directions = dir
 	return view
 }
 
 func (this *GView) Enable() {
-	text := ([]byte) (this.name + "'s move (r, p, s):  ")
+	text := ([]byte) (instr)
 	if _, err := this.inOut.Write(text); err != nil {
 		fmt.Fprintln(os.Stderr, "Error Writing To Stream (" + this.name + ")")
 	}
@@ -48,17 +57,19 @@ func(this *GView) Set(move interface{}) {
 func(this *GView) Get() interface{} {
 if this == nil {
 	return nil
-) else {
+} else {
 	buffer := make([]byte, 2048)
 		tempString := ""
-		n, _ := inOut.Read(buffer)
+		n, _ := this.inOut.Read(buffer)
 		for {
-			if n <= 0 { break; }
-			tempString = tempString + ((string)buffer[0:n])
-			n, _ = inOut.Read(buffer)
+			if n <= 0 { return tempString }
+			temp := (string) (buffer[0:n])
+			tempString = tempString + temp
+			n, _ = this.inOut.Read(buffer)
 		}
 		return tempString
 	}
+return ""
 }
 
 func (this *GView) Loop() os.Error {
@@ -69,21 +80,67 @@ func (this *GView) Loop() os.Error {
 		case "enable": this.Enable()
 		case "get": {
 			val := this.Get()
-			switch reflect.Typeof(val){ 
+			switch val.(string) { 
 			//is an os.Error
-			case *reflect.StructType: return val
-			case *reflect.StringType: this.refComm <- val.(string)
+			case "ICLOSED": return os.NewError("Input stream has been closed")
+			case "OCLOSED": return os.NewError("Output stream has been closed")
+			default: this.refComm <- val.(string)
 			}
 		}
+		case "other": this.Set(<- this.refComm)
 		case "display": this.Display()
+		case "result":{
+			val := this.refComm
+			intVal, _ := strconv.Atoi(val)
+			this.Done(intVal)
+		}
 		case "quit": done = true
+	}
+	return nil
+}
+
+func(this *GView) Display() {
+	this.inOut.Write(([]byte) (this.other))
+}
+
+func(this *GView) Done(youWin game.Outcome) {
+	if youWin == game.Win {
+		this.inOut.Write(([]byte) (this.name + " won."))
+	} else if youWin == game.Draw {
+		this.inOut.Write(([]byte) ("There was a tie."))
+	} else {
+		this.inOut.Write(([]byte) (this.name + " lost."))
+	}
+}
+
+//RPS game logic
+
+const( 	R = 1
+	P = 2
+	S = 3
+	)
+
+//winMove is true if x wins
+bool tieMove = false
+bool winMove = false
+
+if [x] == [o] {
+	tieMove = true
+} else {
+	if [o] == R && [x] == P {
+		winMove == ture
+	} if [o] == P && [x] == S {
+		winMove == ture
+	} if [o] == S && [x] == R {
+		winMove == ture
+	}
+}
+
 		}
 	}
 	return nil
 }
-			
-func main() {
-	view := NewGView(os.Stdout, "A")
-	go view.Loop()
-	//println(view.Get().(string))
+
+func(this *GView) Display() {
+	//display opponent's move
 }
