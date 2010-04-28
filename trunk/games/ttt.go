c3949a37285207eab8f0288a2685dbbca1d6d786
hegemon.bill <hegemon.bill@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-28 21:54:55 +0000

    Almost working

--- trunk/games/ttt.go
+++ trunk/games/ttt.go
@@ -17,11 +17,12 @@ import "os"
 import "strconv"
 import "view"
 import "strings"
+import "container/vector"
 			
 var (
 	x int = 3
 	y int = 3
-	mtx []*vector.Vector
+	mtx []*vector.StringVector
 	full int = 0
 )
 
@@ -50,10 +51,10 @@ func main() {
 	stillPlaying := true
 
 	//makes gameboard
-	mtx = make([] *vector.Vector, x)
+	mtx = make([] *vector.StringVector, x)
 	//populates gameboard
 	for n, _ := range mtx {
-		mtx[n] = new(vector.Vector)
+		mtx[n] = new(vector.StringVector)
 		for b := 0; b < y; b++ {
 			mtx[n].Push("")
 		}
@@ -82,16 +83,16 @@ func main() {
 		winA := winner("A")
 		winB := winner("B")
 		if winA == games.Lose && winB == games.Lose && full == 9 {
-			resA = strconv.Itoa((int) games.Draw) + "\n"
-			resB = strconv.Itoa((int) games.Draw) + "\n"
+			resA = strconv.Itoa((int) (games.Draw)) + "\n"
+			resB = strconv.Itoa((int) (games.Draw)) + "\n"
 			stillPlaying = false
 		} else if winA == games.Win {
-			resA = strconv.Itoa((int) games.Win) + "\n"
-			resB = strconv.Itoa((int) games.Lose) + "\n"
+			resA = strconv.Itoa((int) (games.Win)) + "\n"
+			resB = strconv.Itoa((int) (games.Lose)) + "\n"
 			stillPlaying = false
 		} else if winB == games.Win {
-			resA = strconv.Itoa((int) games.Win) + "\n"
-			resB = strconv.Itoa((int games.Lose) + "\n"
+			resA = strconv.Itoa((int) (games.Win)) + "\n"
+			resB = strconv.Itoa((int) (games.Lose)) + "\n"
 			stillPlaying = false
 		}
 
@@ -105,7 +106,7 @@ func main() {
 		
 			if setGame(aMove, "B") != nil {
 				//tell B it's move was bad
-			//GO BACK and repeat B's move
+				//GO BACK and repeat B's move
 				goto BMOVE
 			}
 
@@ -118,16 +119,16 @@ func main() {
 			winA := winner("A")
 			winB := winner("B")
 			if winA == games.Lose && winB == games.Lose && full == 9 {
-				resA = strconv.Itoa((int) games.Draw) + "\n"
-				resB = strconv.Itoa((int) games.Draw) + "\n"
+				resA = strconv.Itoa((int) (games.Draw)) + "\n"
+				resB = strconv.Itoa((int) (games.Draw)) + "\n"
 				stillPlaying = false
 			} else if winA == games.Win {
-				resA = strconv.Itoa((int) games.Win) + "\n"
-				resB = strconv.Itoa((int) games.Lose) + "\n"
+				resA = strconv.Itoa((int) (games.Win)) + "\n"
+				resB = strconv.Itoa((int) (games.Lose)) + "\n"
 				stillPlaying = false
 			} else if winB == games.Win {
-				resA = strconv.Itoa((int) games.Win) + "\n"
-				resB = strconv.Itoa((int games.Lose) + "\n"
+				resA = strconv.Itoa((int) (games.Win)) + "\n"
+				resB = strconv.Itoa((int) (games.Lose)) + "\n"
 				stillPlaying = false
 			}
 		}	
@@ -140,70 +141,70 @@ func main() {
 
 func setGame(m, p string) os.Error {
 	switch m {	
-		case "n": {
+		case "n" : {
 			if mtx[0].At(1) != "" {
 				return os.NewError("Space Filled!")
 			} else {
 					mtx[0].Set(1, p)
 			}
 		}
-		case "nw"
+		case "nw" : {
 			if mtx[0].At(0) != "" {
 				return os.NewError("Space Filled!")
 			} else {
 					mtx[0].Set(0, p)
 			}
 		}
-		case "w"
+		case "w" : {
 			if mtx[1].At(0) != "" {
 				return os.NewError("Space Filled!")
 			} else {
 					mtx[1].Set(0, p)
 			}
 		}
-		case "sw"
+		case "sw" : {
 			if mtx[2].At(0) != "" {
 				return os.NewError("Space Filled!")
 			} else {
 					mtx[2].Set(0, p)
 			}
 		}
-		case "s"
+		case "s" : {
 			if mtx[2].At(1) != "" {
 				return os.NewError("Space Filled!")
 			} else {
 					mtx[2].Set(1, p)
 			}
 		}
-		case "se"
+		case "se" : {
 			if mtx[2].At(2) != "" {
 				return os.NewError("Space Filled!")
 			} else {
 					mtx[2].Set(2, p)
 			}
 		}
-		case "e"
+		case "e" : {
 			if mtx[1].At(2) != "" {
 				return os.NewError("Space Filled!")
 			} else {
 					mtx[1].Set(2, p)
 			}
 		}
-		case "ne"
+		case "ne" : {
 			if mtx[0].At(2) != "" {
 				return os.NewError("Space Filled!")
 			} else {
 					mtx[0].Set(2, p)
 			}
 		}
-		case "c"
+		case "c" : {
 			if mtx[1].At(1) != "" {
 				return os.NewError("Space Filled!")
 			} else {
 					mtx[1].Set(1, p)
 			}
 		}
-		default: {
+		default : {
 			return os.NewError("Illegal Move")
 			full--
 		}
@@ -216,16 +217,19 @@ func setGame(m, p string) os.Error {
 func winner(name string) games.Outcome {
 	//TTT GAME LOGIC
 
-	bool winner = false
-	for ow = 0; row < x; row++
-		if (mtx[rw].At(0) == name && mtx[rw].At(1) == name && mtx[rw].At(2) == name) {
+	winner := false
+	for row := 0; row < x; row++ {
+		if (mtx[row].At(0) == name && mtx[row].At(1) == name && mtx[row].At(2) == name) {
 			winner = true
-		} if (mtx[0].At(rw) == name && mtx[1].At(rw) == name && mtx[2].At(rw) == name) {
+		}
+		if (mtx[0].At(row) == name && mtx[1].At(row) == name && mtx[2].At(row) == name) {
 			winner = true
+		}
 	}
 	if (mtx[0].At(0) == name && mtx[1].At(1) == name && mtx[2].At(2) == name) {
 			winner = true
-	}	if (mtx[2].At(0) == name && mtx[1].At(1) == name && mtx[0].At(2) == name) {
+	}
+	if (mtx[2].At(0) == name && mtx[1].At(1) == name && mtx[0].At(2) == name) {
 			winner = true
 	}
 
