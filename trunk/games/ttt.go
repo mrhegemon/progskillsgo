478feefdcc18875f8a1e851cdf2fd5cac4b9e57f
hegemon.bill <hegemon.bill@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-28 22:59:00 +0000

    Totally works

--- trunk/games/ttt.go
+++ trunk/games/ttt.go
@@ -30,8 +30,7 @@ func main() {
 
 	term := os.Stdin
 	var err os.Error
-	
-	println(strconv.Itoa(len(os.Args)))
+
 	if len(os.Args) > 1 {
 		term, err = os.Open(os.Args[1], os.O_RDWR, 511)
 		if err != nil { term = os.Stdin }
@@ -47,96 +46,98 @@ func main() {
 	
 	go a.Loop()
 	go b.Loop()
+	for {
+		stillPlaying := true
 	
-	stillPlaying := true
-
-	//makes gameboard
-	mtx = make([] *vector.StringVector, x)
-	//populates gameboard
-	for n, _ := range mtx {
-		mtx[n] = new(vector.StringVector)
-		for b := 0; b < y; b++ {
-			mtx[n].Push("")
-		}
-	}
-
-	resA, resB := "", ""
-
-	for stillPlaying {
-
-		AMOVE: aComm <- "enable"
-		aComm <- "get"
-
-		aMove := strings.TrimSpace(<- aComm)
-		if aMove == "q" { bComm <- "quit" }
-
-		if setGame(aMove, "A") != nil {
-			//tell A it's move was bad
-			//GO BACK and repeat A's move
-			goto AMOVE
-		}
-		bComm <- "other"
-		bComm <- "A's move:  " + aMove + "\n"
-		bComm <- "display"
-
-		//check for win state
-		winA := winner("A")
-		winB := winner("B")
-		if winA == games.Lose && winB == games.Lose && full == 9 {
-			resA = strconv.Itoa((int) (games.Draw)) + "\n"
-			resB = strconv.Itoa((int) (games.Draw)) + "\n"
-			stillPlaying = false
-		} else if winA == games.Win {
-			resA = strconv.Itoa((int) (games.Win)) + "\n"
-			resB = strconv.Itoa((int) (games.Lose)) + "\n"
-			stillPlaying = false
-		} else if winB == games.Win {
-			resA = strconv.Itoa((int) (games.Win)) + "\n"
-			resB = strconv.Itoa((int) (games.Lose)) + "\n"
-			stillPlaying = false
-		}
-
-		if stillPlaying {
-			BMOVE: bComm <- "enable"
-
-			bComm <- "get"
-		
-			bMove := strings.TrimSpace(<- bComm) 
-			if bMove == "q" { aComm <- "quit" }
-		
-			if setGame(aMove, "B") != nil {
-				//tell B it's move was bad
-				//GO BACK and repeat B's move
-				goto BMOVE
+		//makes gameboard
+		mtx = make([] *vector.StringVector, x)
+		//populates gameboard
+		for n, _ := range mtx {
+			mtx[n] = new(vector.StringVector)
+			for b := 0; b < y; b++ {
+				mtx[n].Push("")
 			}
-
-			aComm <- "other"
-			aComm <- "B's move:  " + bMove + "\n"
-
-			aComm <- "display"
-
+		}
+	
+		var resA, resB string = "", ""
+	
+		for stillPlaying {
+	
+			AMOVE: aComm <- "enable"
+			aComm <- "get"
+	
+			aMove := strings.TrimSpace(<- aComm)
+			if aMove == "q" { bComm <- "quit" }
+	
+			if setGame(aMove, "A") != nil {
+				//tell A it's move was bad
+				//GO BACK and repeat A's move
+				goto AMOVE
+			}
+			bComm <- "other"
+			bComm <- "A's move:  " + aMove + "\n"
+			bComm <- "display"
+	
 			//check for win state
 			winA := winner("A")
 			winB := winner("B")
 			if winA == games.Lose && winB == games.Lose && full == 9 {
-				resA = strconv.Itoa((int) (games.Draw)) + "\n"
-				resB = strconv.Itoa((int) (games.Draw)) + "\n"
+				resA = strconv.Itoa((int) (games.Draw))
+				resB = strconv.Itoa((int) (games.Draw))
 				stillPlaying = false
 			} else if winA == games.Win {
-				resA = strconv.Itoa((int) (games.Win)) + "\n"
-				resB = strconv.Itoa((int) (games.Lose)) + "\n"
+				resA = strconv.Itoa((int) (games.Win))
+				resB = strconv.Itoa((int) (games.Lose))
 				stillPlaying = false
 			} else if winB == games.Win {
-				resA = strconv.Itoa((int) (games.Win)) + "\n"
-				resB = strconv.Itoa((int) (games.Lose)) + "\n"
+				resA = strconv.Itoa((int) (games.Win)) 
+				resB = strconv.Itoa((int) (games.Lose))
 				stillPlaying = false
 			}
-		}	
+	
+			if stillPlaying {
+				BMOVE: bComm <- "enable"
+	
+				bComm <- "get"
+			
+				bMove := strings.TrimSpace(<- bComm) 
+				if bMove == "q" { aComm <- "quit" }
+			
+				if setGame(bMove, "B") != nil {
+					//tell B it's move was bad
+					//GO BACK and repeat B's move
+					goto BMOVE
+				}
+	
+				aComm <- "other"
+				aComm <- "B's move:  " + bMove + "\n"
+	
+				aComm <- "display"
+	
+				//check for win state
+				winA := winner("A")
+				winB := winner("B")
+				if winA == games.Lose && winB == games.Lose && full == 9 {
+					resA = strconv.Itoa((int) (games.Draw))
+					resB = strconv.Itoa((int) (games.Draw))
+					stillPlaying = false
+				} else if winA == games.Win {
+					resA = strconv.Itoa((int) (games.Win))
+					resB = strconv.Itoa((int) (games.Lose))
+					stillPlaying = false
+				} else if winB == games.Win {
+					resA = strconv.Itoa((int) (games.Win))
+					resB = strconv.Itoa((int) (games.Lose))
+					stillPlaying = false
+				}
+			}	
+		}
+	
+		aComm <- "result"
+		bComm <- "result"
+		aComm <- resA
+		bComm <- resB
 	}
-	aComm <- "result"
-	bComm <- "result"
-	aComm <- resA
-	bComm <- resB
 }
 
 func setGame(m, p string) os.Error {
