f89e84f96ef1eae63ae688b80875dab077572b4f
tymlipari@yahoo.com <tymlipari@yahoo.com@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-04-28 19:17:17 +0000

    Finished generic view. stored in Tview.go

--- games/rps.go
+++ games/rps.go
@@ -1,11 +1,16 @@
+                                                                     
+                                                                     
+                                                                     
+                                             
 /*
 Authors: William Broza, Tym Lipari
-Matrix Testing program
+Rock Paper Scissors game
 
-Written in a pattern of parallel turntaking.
+Written in a pattern of serial turn taking.  TTT must extend 
+asymmetrically when using channels over a network.
 
 usage:
-	matrix
+	rps-stdin /dev/ttys000 or /dev/ttys001
 */
 
 package main
@@ -14,75 +19,17 @@ package main
 import "os"
 import "io"
 import "fmt"
-import "reflect"
 
 type GView struct {
 	inOut io.ReadWriter
 	name, other string
 	refComm chan string
-}
-
-//bool gametype = false; //false for RPS, True for TTT
-
-func NewGView(inout io.ReadWriter, n string, ref chan string) *RPSView {
-	view := new(GView)
-	view.inOut = inout
-	view.name = n
-	view.other = ""
-	view.refComm = ref
-	return view
-}
-
-func (this *GView) Enable() {
-	text := ([]byte) (this.name + "'s move (r, p, s):  ")
-	if _, err := this.inOut.Write(text); err != nil {
-		fmt.Fprintln(os.Stderr, "Error Writing To Stream (" + this.name + ")")
-	}
-}
-
-func(this *GView) Set(move interface{}) {
-	this.other = move.(string)
-}
-
-func(this *GView) Get() interface{} {
-if this == nil {
-	return nil
-) else {
-	buffer := make([]byte, 2048)
-		tempString := ""
-		n, _ := inOut.Read(buffer)
-		for {
-			if n <= 0 { break; }
-			tempString = tempString + ((string)buffer[0:n])
-			n, _ = inOut.Read(buffer)
-		}
-		return tempString
-	}
-}
-
-func (this *GView) Loop() os.Error {
-	done := false
-	for !done {
-		command := <- this.refComm
-		switch command {
-		case "enable": this.Enable()
-		case "get": {
-			val := this.Get()
-			switch reflect.Typeof(val){ 
-			//is an os.Error
-			case *reflect.StructType: return val
-			case *reflect.StringType: this.refComm <- val.(string)
-			}
-		}
-		case "display": this.Display()
-		case "quit": done = true
-		}
-	}
-	return nil
+	instr chan string
 }
 			
 func main() {
-	view := NewGView(os.Stdout, "A")
+	commChan := make(chan string)
+	view := NewGView(os.Stdout, "A", commChan)
 	go view.Loop()
 	//println(view.Get().(string))
 }
--- games/ttt.go
+++ games/ttt.go
@@ -6,7 +6,7 @@ Written in a pattern of serial turn taking.  TTT must extend
 asymmetrically when using channels over a network.
 
 usage:
-	matrix
+	ttt-stdin /dev/ttys000 or /dev/ttys001
 */
 
 package main
@@ -20,77 +20,14 @@ type GView struct {
 	inOut io.ReadWriter
 	name, other string
 	refComm chan string
-}
-
-//bool gametype = false; //false for RPS, True for TTT
-
-func NewGView(inout io.ReadWriter, n string, ref chan string) *GView {
-	view := new(GView)
-	view.inOut = inout
-	view.name = n
-	view.other = ""
-	view.refComm = ref
-	return view
-}
-
-func (this *GView) Enable() {
-	text := ([]byte) (this.name + "'s move (r, p, s):  ")
-	if _, err := this.inOut.Write(text); err != nil {
-		fmt.Fprintln(os.Stderr, "Error Writing To Stream (" + this.name + ")")
-	}
-}
-
-func(this *GView) Set(move interface{}) {
-	this.other = move.(string)
-}
-
-func(this *GView) Get() interface{} {
-if this == nil {
-	return nil
-} else {
-	buffer := make([]byte, 2048)
-		tempString := ""
-		n, _ := this.inOut.Read(buffer)
-		for {
-			if n <= 0 { return tempString }
-			temp := (string) (buffer[0:n])
-			tempString = tempString + temp
-			n, _ = this.inOut.Read(buffer)
-		}
-		return tempString
-	}
-return ""
-}
-
-func (this *GView) Loop() os.Error {
-	done := false
-	for !done {
-		command := <- this.refComm
-		switch command {
-		case "enable": this.Enable()
-		case "get": {
-			val := this.Get()
-			switch val.(string) { 
-			//is an os.Error
-			case "ICLOSED": return os.NewError("Input stream has been closed")
-			case "OCLOSED": return os.NewError("Output stream has been closed")
-			default: this.refComm <- val.(string)
-			}
-		}
-		case "display": this.Display()
-		case "quit": done = true
-		}
-	}
-	return nil
-}
-
-func(this *GView) Display() {
-	//display opponent's move
+	instr chan string
 }
 			
 func main() {
+
 	commChan := make(chan string)
 	view := NewGView(os.Stdout, "A", commChan)
 	go view.Loop()
-	//println(view.Get().(string))
+
+	println(view.Get().(string))
 }
