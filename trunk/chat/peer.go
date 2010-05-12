b1aa9e982db460b1c102377daf7658be9fd47f89
tymlipari@yahoo.com <tymlipari@yahoo.com@7e0d6034-a87d-3e28-8e87-d693ca8263c6>
2010-05-12 12:32:35 +0000

    added peer.go

---
+++ trunk/chat/peer.go
@@ -0,0 +1,42 @@
+package chat
+
+import("websocket")
+
+type Peer struct {
+	msgs chan string
+	table PeerTable
+	id int
+	conn *websocket.Conn
+	sync.Mutex
+}
+
+func(this *Peer) sendMessage(message string) {
+	msgs <- message
+}
+
+func(this *Peer) printLoop() {
+	for {
+		message := <- msgs
+		_, err := conn.Write([]byte(message))
+		if err != nil {
+			table.removePeer(id)
+			conn.Close()
+			break
+		}
+	}
+}
+
+func(this *Peer) readLoop() {
+	for {
+		reader := bufio.NewReader(conn)
+		message, err := reader.ReadString("\n")
+		if err != nil {
+			println(err.String())
+			conn.Close()
+			table.removePeer(id)
+			break
+		} else {
+			table.sendMessageToAll(string(message), id)
+		}
+	}
+}
